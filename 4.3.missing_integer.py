2d8b8340f1b33dc1e456887470662d684c4cc37c
Davi <davi.wl@gmail.com>
2020-06-05 14:36:06 -0300

    missing integer

---
+++ 4.3.missing_integer.py
@@ -0,0 +1,35 @@
+'''This is a demo task.
+
+Write a function:
+
+def solution(A)
+
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+Given A = [1, 2, 3], the function should return 4.
+
+Given A = [−1, −3], the function should return 1.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].'''
+def solution(A):
+    index = 0
+    try:
+        A.sort()
+        index = A.index(1)
+    except:
+        return 1
+    counter =1
+    for elem in A[index:]:
+        if elem > counter:
+            return counter
+        if elem == counter:
+            counter +=1
+    return counter
+
+
+
