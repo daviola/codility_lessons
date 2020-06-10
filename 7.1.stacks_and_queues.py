9d7837f9b7cbedab37b3cd53f08397df8edca76a
Davi <davi.wl@gmail.com>
2020-06-10 16:25:00 -0300

    Lesson 7.1 Brackets

---
+++ 7.1.stacks_and_queues.py
@@ -0,0 +1,41 @@
+'''A string S consisting of N characters is considered to be properly nested if any of the following conditions is true:
+
+S is empty;
+S has the form "(U)" or "[U]" or "{U}" where U is a properly nested string;
+S has the form "VW" where V and W are properly nested strings.
+For example, the string "{[()()]}" is properly nested but "([)()]" is not.
+
+Write a function:
+
+def solution(S)
+
+that, given a string S consisting of N characters, returns 1 if S is properly nested and 0 otherwise.
+
+For example, given S = "{[()()]}", the function should return 1 and given S = "([)()]", the function should return 0, as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [0..200,000];
+string S consists only of the following characters: "(", "{", "[", "]", "}" and/or ")".'''
+def solution(S):
+    opens = ['(', '{','[']
+    closes = {'(':')', '{':'}','[':']'}
+    brackets =[]
+    for char in S:
+        if char in opens:
+            brackets.append(char)
+        else:
+            if brackets:
+                if closes[brackets[-1]] == char:
+                    brackets.pop(-1)
+                else:
+                    return 0
+            else:
+                return 0                         
+    if not brackets:
+        return 1
+    return 0
+
+print(solution("{[()()]}"))
+print(solution("([)()]"))
+
